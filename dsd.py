4841d280f8d649820a8c7508b8936f64bfd1de15
shamimislamonline@gmail.com <shamimislamonline@gmail.com>
2021-09-13 12:39:47 +0600

    update

--- dsd.py
+++ dsd.py
@@ -1,21 +1,9 @@
 
-'''
 
-for i in range(1, 20, 1):
-     n = int(input("Enter your Last number : "))
-     for x in range(n ,0 , -1):
-        print((2*x-1)*"*") 
+bank = (["ff", "fjjf", "sdf"])
 
-
-'''
-
-
-sum = int(input("Enter A Number :: "))
-
-for i in range(1, sum+1) :
-    print((sum-i) * " ", end="")
-    print(i * "*")
-
+bank.popleft()
+print(bank)
 
 
 
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ n = input("Enter a text of number : ")
 num = n.split()
 sum = 0
 
-for x in num:
-    sum = sum+int(x)
+for s in num:
+    sum = sum+int(s)
+
+#shamim
 
 
 print(sum)
